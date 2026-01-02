994c87cea41b8900dc0262768ebf771c5d1364e5
김형진 <66249379+rlagudwls5518@users.noreply.github.com>
2026-01-02 15:50:50 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 92.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        char[]str = message.toCharArray();
+        
+        answer = str.length * 2;
+        
+        return answer;
+    }
+}
