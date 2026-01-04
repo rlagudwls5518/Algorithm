a65444f2d595808706f73aae8f18fa0ac3191df6
김형진 <66249379+rlagudwls5518@users.noreply.github.com>
2026-01-04 13:01:13 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.07 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        int answer = my_string.endsWith(is_suffix) ? 1 : 0;
+        
+    
+        return answer;
+    }
+}
