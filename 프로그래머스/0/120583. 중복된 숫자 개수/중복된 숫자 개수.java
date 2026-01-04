41d2d2284fabdd225ee98e884a7f355008618aff
김형진 <66249379+rlagudwls5518@users.noreply.github.com>
2026-01-04 11:14:22 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.02 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        
+        for(int arr : array){
+            if(arr == n){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
