effbf5f8423aaad4fc212d2dd38ce38280c2ed27
김형진 <66249379+rlagudwls5518@users.noreply.github.com>
2026-01-18 17:29:55 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14616 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+    static void BFS(int startX, int startY, int N, int M) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{startX, startY});
+        visited[startX][startY] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
+                    if (map[nx][ny] == 1 && !visited[nx][ny]) {
+                        visited[nx][ny] = true;
+                        map[nx][ny] = map[x][y] + 1;
+                        q.add(new int[]{nx, ny});
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j) - '0';
+
+            }
+        }
+        BFS(0, 0, N, M);
+
+        System.out.println(map[N - 1][M - 1]);
+    }
+}
