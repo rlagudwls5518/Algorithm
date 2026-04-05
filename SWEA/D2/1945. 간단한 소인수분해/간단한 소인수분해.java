305afc8bd4a5b4d4842c66840f9a616d6e27de89
김형진 <66249379+rlagudwls5518@users.noreply.github.com>
2026-04-05 17:36:58 +0900

    [D2] Title: 간단한 소인수분해, Time: 76 ms, Memory: 25,344 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
@@ -27,7 +27,7 @@
 /////////////////////////////////////////////////////////////////////////////////////////////
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
+import java.util.StringTokenizer;
 /*
    사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
    이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
@@ -39,13 +39,14 @@ class Solution
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int T;
-        T = Integer.parseInt(br.readLine().trim());
-
+        T = Integer.parseInt(st.nextToken());
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
             result = new int[5];
             for (int j = 4; j >= 0; j--) {
                 while (N % numbers[j] == 0) {
