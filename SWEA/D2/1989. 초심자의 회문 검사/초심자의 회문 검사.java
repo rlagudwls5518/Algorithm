fec30e0c6f4b63f70fb016b8e3bbdc63dcdab98d
김형진 <66249379+rlagudwls5518@users.noreply.github.com>
2026-04-14 17:30:07 +0900

    [D2] Title: 초심자의 회문 검사, Time: 76 ms, Memory: 24,704 KB -BaekjoonHub

---
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -0,0 +1,72 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T;
+        T = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            st = new StringTokenizer(br.readLine());
+
+            char[] P = st.nextToken().toCharArray();
+            int result = 1;
+
+            int leftIndex = 0;
+            int rightIndex = P.length - 1;
+
+            while(leftIndex < rightIndex){
+                if(P[leftIndex] == P[rightIndex]){
+                    leftIndex++;
+                    rightIndex--;
+                }
+                else{
+                    result = 0;
+                    break;
+                }
+            }
+
+            sb.append("#" + test_case + " ");
+            sb.append(result).append("\n");
+        }
+        System.out.println(sb);
+	}
+}
